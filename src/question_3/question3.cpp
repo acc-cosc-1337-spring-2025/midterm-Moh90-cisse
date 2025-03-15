9cfab73a05e456570118e7f537338387ea35b1db
Moh90-cisse <mohamed.cisse@g.austincc.edu>
2025-03-15 14:04:26 -0500

    Update question3.cpp

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,6 +1,7 @@
-#include "question3.h"
+#include "parameters_demo.h"
 
-bool test_config()
-{
-    return true;
+// Function to demonstrate how value and reference parameters work
+void show_value_and_reference_parameters(int valueParam, int& referenceParam) {
+    valueParam += 5; // Changes to valueParam will not affect the original argument
+    referenceParam += 10; // Changes to referenceParam will affect the original argument
 }
