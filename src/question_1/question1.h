8d873188a9fb6df246a25ad40227380fe039f056
Moh90-cisse <mohamed.cisse@g.austincc.edu>
2025-03-15 14:01:05 -0500

    Update question1.h

--- src/question_1/question1.h
+++ src/question_1/question1.h
@@ -1,1 +1,12 @@
-bool test_config();
+#ifndef PRIME_UTILS_H
+#define PRIME_UTILS_H
+
+#include <vector>
+
+// Function prototype to check if a number is prime
+bool is_prime(int num);
+
+// Function prototype to get all prime numbers up to 'num'
+std::vector<int> get_primes(int num);
+
+#endif // PRIME_UTILS_H
