ce7ffc455c0080fa6da02abe21da82e94653411b
Moh90-cisse <mohamed.cisse@g.austincc.edu>
2025-03-15 14:02:45 -0500

    Update question2.cpp

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,16 @@
-#include "question2.h"
+#include "gcd_utils.h"
 
-bool test_config()
-{
-    return true;
+// Function to find the greatest common denominator (GCD) of two numbers using loops
+int find_gcd(int num1, int num2) {
+    // The GCD cannot be greater than the smaller of the two numbers
+    int gcd = -1;
+    
+    // Loop through all numbers from 1 to the smaller of num1 or num2
+    for (int i = 1; i <= num1 && i <= num2; ++i) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            gcd = i;  // Store the common divisor
+        }
+    }
+
+    return gcd;
 }
