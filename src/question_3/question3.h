f738ca050c6317c92d9a2bc80ac22123cd09e002
Moh90-cisse <mohamed.cisse@g.austincc.edu>
2025-03-15 14:04:05 -0500

    Update question3.h

--- src/question_3/question3.h
+++ src/question_3/question3.h
@@ -1,1 +1,7 @@
-bool test_config();
+#ifndef PARAMETERS_DEMO_H
+#define PARAMETERS_DEMO_H
+
+// Function prototype to demonstrate value and reference parameters
+void show_value_and_reference_parameters(int valueParam, int& referenceParam);
+
+#endif // PARAMETERS_DEMO_H
