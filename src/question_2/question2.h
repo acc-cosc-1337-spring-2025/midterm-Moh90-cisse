28969cd3060436596b8b72d9906357321ae8a812
Moh90-cisse <mohamed.cisse@g.austincc.edu>
2025-03-15 14:02:17 -0500

    Update question2.h

--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,7 @@
-bool test_config();
+#ifndef GCD_UTILS_H
+#define GCD_UTILS_H
+
+// Function prototype to find the greatest common denominator (GCD) of two numbers
+int find_gcd(int num1, int num2);
+
+#endif // GCD_UTILS_H
